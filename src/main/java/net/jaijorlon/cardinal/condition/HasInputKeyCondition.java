f3934951b4649c7e9defb05b5934a547c0ec3e1a
Jaijorlon <jaidenlucas13@gmail.com>
2026-08-21 12:00:38 -0500

    Fixed HasInputKeyCondition using wrong data

--- src/main/java/net/jaijorlon/cardinal/condition/HasInputKeyCondition.java
+++ src/main/java/net/jaijorlon/cardinal/condition/HasInputKeyCondition.java
@@ -22,97 +22,97 @@ public class HasInputKeyCondition extends Condition {
         var entity = context.getEntity();
 
         if (entity != null) {
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.up")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.up")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("forward") || s.equalsIgnoreCase("up"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.down")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.down")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("down") || s.equalsIgnoreCase("backward"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.left")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.left")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("left"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.right")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.right")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("right"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.jump")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.jump")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("jump"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.sprint")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.sprint")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("sprint"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.use")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.use")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("use"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.attack")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.attack")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("attack"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.shift")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.shift")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("shift"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.advancements")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.advancements")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("advancements"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.chat")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.chat")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("chat"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.command")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.command")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("command"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.advancements")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.advancements")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("drop"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.fullscreen")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.fullscreen")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("fullscreen"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.inventory")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.inventory")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("inventory"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.pick_item")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.pick_item")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("pick_item"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.swap_offhand")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.swap_offhand")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("swap_offhand"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.toggle_perspective")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.toggle_perspective")) {
                 if (Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("toggle_perspective"))) {
                     return true;
                 }
             }
-            if (entity.getPersistentData().getBoolean("Untapped.MouseClickCondition.smooth_camera")) {
+            if (entity.getPersistentData().getBoolean("Cardinal.HasInputKeyCondition.smooth_camera")) {
                 return Arrays.stream(this.inputKeys).anyMatch(s -> s.equalsIgnoreCase("smooth_camera"));
             }
         }
